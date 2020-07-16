6dede10f4ce1392f57995b22d62ab13aaae1523b
LÊ VĂN TÁM <letamhot@gmail.com>
2020-07-16 13:40:33 +0700

    ud

--- public/js/style.js
+++ public/js/style.js
@@ -1,54 +1,54 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
 
-function myFunction1() {
-    document.getElementById("dropdown-con").classList.toggle("show");
+// function myFunction1() {
+//     document.getElementById("dropdown-con").classList.toggle("show");
 
-}
+// }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.btn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-contact");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//     if (!event.target.matches('.btn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-contact");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
 
-function myFunction2() {
-    document.getElementById("userDropdown").classList.toggle("show");
-}
+// function myFunction2() {
+//     document.getElementById("userDropdown").classList.toggle("show");
+// }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.userbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-user");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//     if (!event.target.matches('.userbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-user");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
